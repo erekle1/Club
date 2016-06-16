9e767f1c89ceb67b5da3d8e50d5a7f49a3c58045
erekle1 <erekle.k1@gmail.com>
2016-06-16 15:38:43 +0400

    Basic Models and Migrations

---
+++ database/migrations/2016_06_16_112802_create_locales_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code',2);
+            $table->string('title',40);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('locales');
+    }
+}
