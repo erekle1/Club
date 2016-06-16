d1a81c94d0a6f13c94c6e4fe23eaf9e9ecc7d105
erekle1 <erekle.k1@gmail.com>
2016-06-16 17:29:03 +0400

    fix forign key on users table

--- database/migrations/2016_06_16_113019_create_posts_table.php
+++ database/migrations/2016_06_16_113019_create_posts_table.php
@@ -17,7 +17,7 @@ class CreatePostsTable extends Migration
             $table->tinyInteger('status')->unisigned();
             $table->integer('views')->unisigned();
             $table->integer('user_id')->unisgned();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
